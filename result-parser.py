e01ddbc463ee1e8f553423f581e420fc7d0f6241
Cjconry <cjconry@gmail.com>
2020-11-07 13:36:49 -0500

    dont rebuild results each time

--- result-parser.py
+++ result-parser.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from plot import build_plot
 
-build_results()
+#build_results()
 data = pd.read_csv('out.csv')  
 #data= data.drop_duplicates(subset=['Progress'], keep='last')
 build_plot(data)
