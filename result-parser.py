762e90b2b92f232048d2c8d256c00959fade54f2
Cjconry <cjconry@gmail.com>
2020-11-07 13:32:56 -0500

    Initial Commit

---
+++ result-parser.py
@@ -0,0 +1,10 @@
+from result_ETL import build_results
+import pandas as pd
+import matplotlib.pyplot as plt
+from plot import build_plot
+
+build_results()
+data = pd.read_csv('out.csv')  
+#data= data.drop_duplicates(subset=['Progress'], keep='last')
+build_plot(data)
+plt.show() 
